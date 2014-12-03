f412590fcd585a9ee1ba0b1fba68ee2a413aabf3
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-12-02 18:25:23 -0800

    added version number and description

--- DirectObjLoader/Properties/AssemblyInfo.cs
+++ DirectObjLoader/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "Revit Add-In Title" )]
-[assembly: AssemblyDescription( "Revit Add-In Description" )]
+[assembly: AssemblyTitle( "DirectObjLoader" )]
+[assembly: AssemblyDescription( "Revit add-in to load a WaveFront OBJ file into a DirectShape element" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Autodesk Inc." )]
-[assembly: AssemblyProduct( "Revit Add-In Wizard Template Application" )]
-[assembly: AssemblyCopyright( "Copyright 2014 © Jeremy Tammik Autodesk Inc." )]
+[assembly: AssemblyProduct( "DirectObjLoader" )]
+[assembly: AssemblyCopyright( "Copyright 2014 © Eric Boehlke and Jeremy Tammik Autodesk Inc." )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "2015.0.0.0" )]
+[assembly: AssemblyFileVersion( "2015.0.0.0" )]
